69a24daa59dfcaae37bfd67b096bcb3358a5ce45
bondarchukm <59935318+bondarchukm@users.noreply.github.com>
2020-04-15 22:46:09 +0300

    Initial commit

---
+++ src/MainClassTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest {
+    MainClass number = new MainClass();
+
+    @Test
+    public void testGetLocalNumber(){
+        Assert.assertEquals("Ups! The number is not 14!",14, number.getLocalNumber());
+    }
+}

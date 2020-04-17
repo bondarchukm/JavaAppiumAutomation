247c1a5698e81185e2a416a033ea457368e19542
bondarchukm <59935318+bondarchukm@users.noreply.github.com>
2020-04-17 10:56:00 +0300

    hw2

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,4 +2,9 @@ public class MainClass {
     public int getLocalNumber(){
         return 14;
     }
+    private int class_number = 20;
+
+    public int getClassNumber(){
+        return class_number;
+    }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,10 +2,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
-    MainClass number = new MainClass();
+    MainClass localNumber = new MainClass();
+    MainClass classNumber = new MainClass();
 
     @Test
     public void testGetLocalNumber(){
-        Assert.assertEquals("Ups! The number is not 14!", 14, number.getLocalNumber());
+        Assert.assertEquals("The " + localNumber.getLocalNumber() +" is not 14!", 14, localNumber.getLocalNumber());
     }
+
+    @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("The " + classNumber.getClassNumber() + " is less (or equals) then 45" ,classNumber.getClassNumber() > 45);
+     }
 }

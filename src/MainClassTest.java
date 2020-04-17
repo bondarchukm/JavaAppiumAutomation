fed4018a16cd79d8793cecaf211d603c1bde1841
bondarchukm <59935318+bondarchukm@users.noreply.github.com>
2020-04-17 19:22:49 +0300

    hw3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 public class MainClassTest {
     MainClass localNumber = new MainClass();
     MainClass classNumber = new MainClass();
+    MainClass classString = new MainClass();
 
     @Test
     public void testGetLocalNumber(){
@@ -13,5 +14,10 @@ public class MainClassTest {
     @Test
     public void testGetClassNumber(){
         Assert.assertTrue("The " + classNumber.getClassNumber() + " is not bigger then 45" ,classNumber.getClassNumber() > 45);
+     }
+
+     @Test
+    public void testGetClassString(){
+        Assert.assertTrue("String does not contain 'hello' or 'Hello'", classString.getClassString().contains("hello") || classString.getClassString().contains("Hello"));
      }
 }

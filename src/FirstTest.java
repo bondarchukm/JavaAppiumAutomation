e3e55c4b3308e2b3d8f59fe9e3922805e0291ded
bondarchukm <bondarchuk.hh@gmail.com>
2020-05-07 19:13:02 +0300

    Home Work Ex5

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -474,179 +474,179 @@ public class FirstTest {
 //        );
 //    }
 
-//    @Test
-//    public void testSaveTwoArticlesInOneList()
-//    {
-//        // Adding first article to the favourits list
-//
-//        waitForElementAndClick(
-//                By.id("org.wikipedia:id/search_container"),
-//                "Cannot find 'Search Wikipedia' input",
-//                5
-//        );
-//
-//        String first_article_search_line = "Game of Thrones";
-//
-//        waitForElementAndSendKeys(
-//                By.xpath("//*[contains(@text, 'Search…')]"),
-//                first_article_search_line,
-//                "Cannot find search input",
-//                5
-//        );
-//
-//        waitForElementAndClick(
-//                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='" + first_article_search_line +"']"),
-//                "No results found for " + first_article_search_line,
-//                5
-//        );
-//
-//        waitForElementPresent(
-//                By.xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text'][@text='" + first_article_search_line +"']"),
-//                "Cannot find article title",
-//                10
-//        );
-//
-//        waitForElementPresent(
-//                By.id("org.wikipedia:id/view_page_title_text"),
-//                "Cannot find article title",
-//                10
-//        );
-//        waitForElementAndClick(
-//                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
-//                "Cannot find button to open article options",
-//                5
-//        );
-//        waitForElementAndClick(
-//                By.xpath("//*[@text='Add to reading list']"),
-//                "Cannot find option to add article to reading list",
-//                5
-//        );
-//        waitForElementAndClick(
-//                By.id("org.wikipedia:id/onboarding_button"),
-//                "Cannot find 'Got It' tip overlay",
-//                5
-//        );
-//        waitForElementAndClear(
-//                By.id("org.wikipedia:id/text_input"),
-//                "Cannot find input to set name of articles folder",
-//                5
-//        );
-//
-//        String name_of_folder = "My TV-shows";
-//
-//        waitForElementAndSendKeys(
-//                By.id("org.wikipedia:id/text_input"),
-//                name_of_folder,
-//                "Cannot put text into articles folder input",
-//                5
-//        );
-//        waitForElementAndClick(
-//                By.xpath("//*[@text='OK']"),
-//                "Cannot press OK button",
-//                5
-//        );
-//        waitForElementAndClick(
-//                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
-//                "Cannot close article, cannot find X button",
-//                5
-//        );
-//
-//        // Adding second article to the favourits list
-//
-//        waitForElementAndClick(
-//                By.id("org.wikipedia:id/search_container"),
-//                "Cannot find 'Search Wikipedia' input",
-//                5
-//        );
-//
-//        String second_article_search_line = "Friends";
-//
-//        waitForElementAndSendKeys(
-//                By.xpath("//*[contains(@text, 'Search…')]"),
-//                second_article_search_line,
-//                "Cannot find search input",
-//                5
-//        );
-//
-//        waitForElementAndClick(
-//                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='" + second_article_search_line +"']"),
-//                "No results found for " + second_article_search_line,
-//                5
-//        );
-//
-//        waitForElementPresent(
-//                By.xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text'][@text='" + second_article_search_line +"']"),
-//                "Cannot find article title",
-//                10
-//        );
-//
-//        waitForElementPresent(
-//                By.id("org.wikipedia:id/view_page_title_text"),
-//                "Cannot find article title",
-//                10
-//        );
-//        waitForElementAndClick(
-//                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
-//                "Cannot find button to open article options",
-//                5
-//        );
-//        waitForElementAndClick(
-//                By.xpath("//*[@text='Add to reading list']"),
-//                "Cannot find option to add article to reading list",
-//                5
-//        );
-//        waitForElementAndClick(
-//                By.xpath("//*[@class='android.widget.TextView'][@text='" + name_of_folder + "']"),
-//                "Cannot find previously saved list " + name_of_folder,
-//                5
-//        );
-//        waitForElementAndClick(
-//                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
-//                "Cannot close article, cannot find X button",
-//                5
-//        );
-//
-//        // Open previously saved list
-//
-//        waitForElementAndClick(
-//                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
-//                "Cannot find navigation button to My list",
-//                5
-//        );
-//        waitForElementAndClick(
-//                By.xpath("//*[@text='" + name_of_folder + "']"),
-//                "Cannot find created folder",
-//                5
-//        );
-//
-//        // Delete first saved article
-//
-//        swipeElementToLeft(
-//                By.xpath("//*[@text='" + first_article_search_line + "']"),
-//                "Cannot find saved article"
-//        );
-//        waitForElementNotPresent(
-//                By.xpath("//*[@text='" + first_article_search_line + "']"),
-//                "Cannot delete saved article",
-//                5
-//        );
-//
-//        // Verify that second article is still in the list and click on it
-//
-//        waitForElementAndClick(
-//                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + second_article_search_line +"']"),
-//                "Second added article " + second_article_search_line + " not found",
-//                5
-//        );
-//
-//        // Verify that article title is matched to the second search
-//
-//        waitForElementPresent(
-//                By.xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text'][@text='" + second_article_search_line +"']"),
-//                "Article title is not matched to " + second_article_search_line,
-//                10
-//        );
-//    }
+    @Test
+    public void testSaveTwoArticlesInOneList()
+    {
+        // Adding first article to the favourits list
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        String first_article_search_line = "Game of Thrones";
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                first_article_search_line,
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='" + first_article_search_line +"']"),
+                "No results found for " + first_article_search_line,
+                5
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text'][@text='" + first_article_search_line +"']"),
+                "Cannot find article title",
+                10
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                10
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Cannot find button to open article options",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to reading list']"),
+                "Cannot find option to add article to reading list",
+                5
+        );
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find 'Got It' tip overlay",
+                5
+        );
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to set name of articles folder",
+                5
+        );
+
+        String name_of_folder = "My TV-shows";
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                name_of_folder,
+                "Cannot put text into articles folder input",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='OK']"),
+                "Cannot press OK button",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot close article, cannot find X button",
+                5
+        );
+
+        // Adding second article to the favourits list
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        String second_article_search_line = "Friends";
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                second_article_search_line,
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='" + second_article_search_line +"']"),
+                "No results found for " + second_article_search_line,
+                5
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text'][@text='" + second_article_search_line +"']"),
+                "Cannot find article title",
+                10
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                10
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Cannot find button to open article options",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to reading list']"),
+                "Cannot find option to add article to reading list",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@class='android.widget.TextView'][@text='" + name_of_folder + "']"),
+                "Cannot find previously saved list " + name_of_folder,
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot close article, cannot find X button",
+                5
+        );
+
+        // Open previously saved list
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Cannot find navigation button to My list",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + name_of_folder + "']"),
+                "Cannot find created folder",
+                5
+        );
+
+        // Delete first saved article
+
+        swipeElementToLeft(
+                By.xpath("//*[@text='" + first_article_search_line + "']"),
+                "Cannot find saved article"
+        );
+        waitForElementNotPresent(
+                By.xpath("//*[@text='" + first_article_search_line + "']"),
+                "Cannot delete saved article",
+                5
+        );
+
+        // Verify that second article is still in the list and click on it
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + second_article_search_line +"']"),
+                "Second added article " + second_article_search_line + " not found",
+                5
+        );
+
+        // Verify that article title is matched to the second search
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text'][@text='" + second_article_search_line +"']"),
+                "Article title is not matched to " + second_article_search_line,
+                10
+        );
+    }
 
 
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
